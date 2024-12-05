7915ee32fae3884e92634c26a5992265b7a5042e
Kristoff Paulson <kpaulson@cfa.harvard.edu>
2024-12-05 13:41:30 -0500

    Updated the plot to have labels and to rotate the allCollector panel setup to be similar to the actual cup in the chamber

--- fcp_realTimePlot.py
+++ fcp_realTimePlot.py
@@ -6,14 +6,22 @@ import pandas as pd
 import shutil
 import tempfile
 import os
+import pathlib
+import os.path
 
 # Change this to the correct data directory for real-time file, or set to '' and work from the directory
-dataDir = 'G:/Shared drives/HelioSwarm/KP_Working/testingTemp/realTimePlot_working/'
+dataDir = 'C:/Users/solarcup/Desktop/data/'
+dataDir = 'C:/Users/solarcup/Box/Helioswarm/05-HSFC-ENGINEERING/10-LAB NOTES TESTING/DEC-2024 lab test data/data/'
+#print([f for f in pathlib.Path(dataDir).glob('data_*.csv')])
+filename = max([f for f in pathlib.Path(dataDir).glob('data_*.csv')], 
+               key=os.path.getctime)
+
+print(filename)
 
 def update_plot(freq):
     # Create a temporary file
     with tempfile.NamedTemporaryFile(delete=False, suffix='.csv') as temp_file:
-        shutil.copyfileobj(open(dataDir+'ADC_dataInput.csv', 'rb'), temp_file)
+        shutil.copyfileobj(open(filename, 'rb'), temp_file)
     
     # Get the user-defined variables
     dataLength     = dataLength_var.get()
@@ -22,24 +30,44 @@ def update_plot(freq):
 
     # Read the last few lines of the temp file into a dataframe
     df = pd.read_csv(temp_file.name).tail(dataLength)
+    
+    # Define plot orientations
+    plotOrder_topDownInTank = [1, 0, 2, 3]
+    plotOrder_gainOrder = [0, 1, 2, 3]
 
     # Select the appropriate columns based on the configuration
     if config == 'Gain0 (all collectors)':
         cols = [0, 4, 8, 12]
+        labels = ['A gain0', 'B gain0', 'C gain0', 'D gain0']
+        plotOrder = plotOrder_topDownInTank
     elif config == 'Gain1 (all collectors)':
         cols = [1, 5, 9, 13]
+        labels = ['A gain1', 'B gain1', 'C gain1', 'D gain1']
+        plotOrder = plotOrder_topDownInTank
     elif config == 'Gain2 (all collectors)':
         cols = [2, 6, 10, 14]
+        labels = ['A gain2', 'B gain2', 'C gain2', 'D gain2']
+        plotOrder = plotOrder_topDownInTank
     elif config == 'Gain3 (all collectors)':
         cols = [3, 7, 11, 15]
+        labels = ['A gain3', 'B gain3', 'C gain3', 'D gain3']
+        plotOrder = plotOrder_topDownInTank
     elif config == 'AllGain (A)':
         cols = [0, 1, 2, 3]
+        labels = ['A gain0', 'A gain1', 'A gain2', 'A gain3']
+        plotOrder = plotOrder_gainOrder
     elif config == 'AllGain (B)':
         cols = [4, 5, 6, 7]
+        labels = ['B gain0', 'B gain1', 'B gain2', 'B gain3']
+        plotOrder = plotOrder_gainOrder
     elif config == 'AllGain (C)':
         cols = [8, 9, 10, 11]
+        labels = ['C gain0', 'C gain1', 'C gain2', 'C gain3']
+        plotOrder = plotOrder_gainOrder
     elif config == 'AllGain (D)':
         cols = [12, 13, 14, 15]
+        labels = ['D gain0', 'D gain1', 'D gain2', 'D gain3']
+        plotOrder = plotOrder_gainOrder
 
     # Isolate relevant columns and ensure numeric data types
     df_subset = df.iloc[:, cols]#.values.reshape(-1, 4)
@@ -51,9 +79,9 @@ def update_plot(freq):
 
     # Plot the data in a 2x2 grid
     for i in range(4):
-        axes[i//2, i%2].plot(df_subset.iloc[:, i])
-        axes[i//2, i%2].set_xlabel('record')
-        axes[i//2, i%2].set_ylabel('value (DN)')
+        axes[i//2, i%2].plot(df_subset.iloc[:, plotOrder[i]])
+        axes[i//2, i%2].set_xlabel('record #')
+        axes[i//2, i%2].set_ylabel('%s (DN)'%labels[plotOrder[i]])
         if fix_yaxisRange:
             axes[i//2, i%2].set_ylim(0, 4096)
 
@@ -68,7 +96,7 @@ def update_plot(freq):
 
 # Create the main window
 root = tk.Tk()
-root.title("FCP Real-time Plot")
+root.title("FCP Real-time Plot - ")
 root.resizable(True, True)
 root.rowconfigure(1, weight=1)
 root.columnconfigure(0, weight=1)
